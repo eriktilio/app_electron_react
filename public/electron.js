1ff78b9439513cf48808d42c1d0160a0c5e7d79a
erik.rey <erik.rey@itriad.org.br>
2024-01-26 16:44:10 -0400

    feat: removing not used files

--- public/electron.js
+++ public/electron.js
@@ -7,18 +7,14 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 function createWindow() {
-    // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        autoHideMenuBar: true,
         webPreferences: { nodeIntegration: true, contextIsolation: false },
     });
-    // and load the index.html of the app.
     console.log(__dirname);
     mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
